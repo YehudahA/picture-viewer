a92e960f7c1f155c3673dce51ec0acdcbe177816
Yehudah A-N <dev.altman@gmail.com>
2022-10-18 15:21:44 +0300

    embed svg's to support stalkblitz

--- src/components/PictureView.js
+++ src/components/PictureView.js
@@ -2,8 +2,11 @@ import React, { useState } from 'react';
 
 import Draggable from './Draggable';
 import styles from './PictureView.module.css';
-import { ReactComponent as PlusSvg } from '../assets/plus.svg';
-import { ReactComponent as MinusSvg } from '../assets/minus.svg';
+
+/* removed for supporting stackblitz
+ import { ReactComponent as PlusSvg } from '../assets/plus.svg';
+ import { ReactComponent as MinusSvg } from '../assets/minus.svg';
+*/
 
 
 export default function PictureView(props) {
@@ -15,7 +18,7 @@ export default function PictureView(props) {
 
   return (
     <div className={styles['img-wrapper']}>
-      
+
       <Draggable>
         <div className={styles['img-drag-wrapper']}>
           <img
@@ -32,11 +35,17 @@ export default function PictureView(props) {
 
       <div className={styles['img-zoom-cmds']}>
         <button onClick={() => setScaleByUnits(10)}>
-          <PlusSvg />
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+            <circle cx="12" cy="12" r="12" />
+            <path d="M 6,12 L 18,12 M 12,6 L 12,18" />
+          </svg>
         </button>
 
         <button onClick={() => setScaleByUnits(-10)}>
-          <MinusSvg />
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+            <circle cx="12" cy="12" r="12" />
+            <path d="M 6,12 L 18,12" />
+          </svg>
         </button>
       </div>
     </div>
